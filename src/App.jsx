4007a3d29faf7448d4825e20e9329aadf243958f
JFlores427 <Flores.Josue427@gmail.com>
2023-10-14 04:34:11 -0400

    Completed some of the required features

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,35 @@ function App() {
   const [banList, setBanList] = useState([]);
   const [data, setData] = useState({});
 
-  const APIRequest = async () => {
-    const popularAnime = await fetch("http://localhost:3000/popular")
-    if(popularAnime) {
-    const popularAnimeJSON = popularAnime.json();
-    }
-    setData({...data, animeId : popularAnimeJSON.animeID, animeTitle: popularAnimeJSON.animeTitle, animeImg: popularAnimeJSON.animeImg});
-
+  const getRandomInt = (max) => {
+    let randomValue = Math.floor(Math.random() * max);
+    return (randomValue !== max) ? randomValue : getRandomInt(max);
   }
+
+  const APIRequest = async () => {
+    const randomPage = getRandomInt(504);
+    const popularAnimeJSON = await fetch("http://localhost:3000/popular?page=" + randomPage).then((response) => response.json());
+
+    const randomIndex = getRandomInt(popularAnimeJSON.length);
+    
+    if(popularAnimeJSON) {
+    setData({...data, animeId : popularAnimeJSON[randomIndex].animeId, animeTitle: popularAnimeJSON[randomIndex].animeTitle, animeImg: popularAnimeJSON[randomIndex].animeImg});
+
+    const animeDetailsJSON = await fetch("http://localhost:3000/anime-details/" + popularAnimeJSON[randomIndex].animeId).then((response) => response.json());
+    //console.log(animeDetailsJSON);
+
+    if(animeDetailsJSON) {
+      //setData({...data, animeGenres: animeDetailsJSON.genres, animeStatus: animeDetailsJSON.status, animeRelease: animeDetailsJSON.releasedDate});  
+      console.log(data);
+    }
+  }
+  }
+
+
+
   return (
     <div className="veni-background">
-      <VeniContainer APIRequest={APIRequest} ></VeniContainer>
+      <VeniContainer APIRequest={APIRequest} data={data} ></VeniContainer>
       {/* <BanList></BanList>
       <History></History> */}
     </div>
--- src/components/VeniContainer.jsx
+++ src/components/VeniContainer.jsx
@@ -2,14 +2,20 @@ import "./VeniContainer.css";
 
 const VeniContainer = (props) => {
 
- const {APIRequest} = props;
+ const {APIRequest, data} = props;
 
+ const animeGenres = [1,2,3,4];
 return (
     <>
     <div className="container">
         <h2 className="title">Veni Vici!</h2>
         <p className="title-description"> Discover Your New Favorite Anime!</p>
-        <button className="explore-button" onClick={APIRequest}>Explore</button>
+        <div className="query-container">
+            <h3 className="query-title">{data.animeTitle}</h3>
+            <div className="query-button-container"> {animeGenres.map((animeGenre) => (<button key={animeGenre} className="banList">{animeGenre}</button>))} </div>
+            <img className="query-image" src={data.animeImg}></img>
+            <button className="explore-button" onClick={APIRequest}>Explore</button>
+        </div>
     </div>
     </>
 )
